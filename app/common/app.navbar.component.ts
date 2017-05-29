23b84ebea24343c87df39aee1894318df967ed6d
grooveymo <ingeniumsoft@gmail.com>
2017-05-29 18:00:54 +0100

    added styling for navbar

--- app/common/app.navbar.component.ts
+++ app/common/app.navbar.component.ts
@@ -1,33 +1,57 @@
 /*
  Defines navigational component
+ Styling from  see https://www.webcodegeeks.com/html5/html5-navigation-bar-example/
+
  */
 import { Component } from '@angular/core';
 @Component({
     selector : 'nav-bar',
-    styles : ['padding : 10px; margin-left: 20px; background-color : green'],
     template : `
-            <div style='border 2px solid red;'>
-            <!--
-               <a (click)="gotoHome($event)" >Home </a> |
-                <a (click)="gotoCreateList()">Create List </a>  |
-                <a (click)="gotoViewLists()">View Lists </a>
- 
-            -->
-                <a routerLink="/home" routerLinkActive="active">Home </a> |
-                <a routerLink="/create-list" routerLinkActive="active">Create List </a>  |
-                <a routerLink="/view-lists" routerLinkActive="active">View Lists </a>
-            </div>
+            <style>
+                nav {
+                    background-color: #333;
+                    border: 1px solid #333;
+                    color: #fff;
+                    display: block;
+                    margin: 0;
+                    overflow: hidden;
+                }
+                nav ul{
+                    margin: 0;
+                    padding: 0;
+                    list-style: none;
+                }
+                nav ul li {
+                    margin: 0;
+                    display: inline-block;
+                    list-style-type: none;
+                    transition: all 0.2s;
+                }
+
+                nav > ul > li > a {
+                    color: #aaa;
+                    /*display: block;*/
+                    line-height: 2em;
+                    padding: 0.5em 2em;
+                    text-decoration: none;
+                }
+            </style>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a routerLink="/home" routerLinkActive="active">Home </a> |
+                    </li>
+                    <li>
+                        <a routerLink="/create-list" routerLinkActive="active">Create List </a>  |
+                    </li>
+                    <li>
+                        <a routerLink="/view-lists" routerLinkActive="active">View Lists </a>
+                    </li>
+                </ul>
+            </nav>
     `
 })
 export class AppNavbarComponent {
-    gotoHome(event : any){
-        console.log('clicked on ' + event.currentTarget);
-    }
-    gotoCreateList(event){
-                console.log('clicked on ' + event);
-    }
-    gotoViewLists(event){
-                console.log('clicked on ' + event);
 
-    }
 }
