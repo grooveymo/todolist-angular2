ed46777bc4a5d230cc0bdcd8c57a37622391bfc6
grooveymo <ingeniumsoft@gmail.com>
2017-05-31 15:33:22 +0100

    implemented filter capability so that we can display all, only active todos, or completed todos

--- app/containers/edit-list-page.component.ts
+++ app/containers/edit-list-page.component.ts
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/switchMap';
 
 import { TodoService } from '../services/todo.service';
 import { TodoList } from '../models/TodoList.interface';
+import { Todo } from '../models/Todo.interface';
 
 @Component({
     selector: 'edit-list-page',
@@ -24,7 +25,8 @@ import { TodoList } from '../models/TodoList.interface';
         </div>
         <div>
             <ul>
-            <li *ngFor='let item of currentTodoList?.todos'>
+            <!-- li *ngFor='let item of currentTodoList?.todos' -->
+            <li *ngFor='let item of displayableTodoList'>
 
                 <div class='todo'>            
                     <input type='checkbox' style='width:5%' [checked]="item.isCompleted" (change)='toggleIsCompleted(currentTodoList._id, item._id)' novalidate/>
@@ -41,19 +43,33 @@ import { TodoList } from '../models/TodoList.interface';
         <div>
             Number of Active Todos: {{numActive}}
         </div>
+        <div>
+            <button (click)='showAll()'> All </button>
+            <button (click)='showActive()'> Active </button>
+            <button (click)='showCompleted()'> Completed </button>
+        </div>
+
     `
 })
 export class EditListPageComponent implements OnInit {
 
     constructor(private router: Router, private route: ActivatedRoute, private todoService: TodoService) { }
 
+    //will hold the actual dataset
     private currentTodoList: TodoList;
+
+    //will hold the data we want to show based on filters: all, active, completed
+    private displayableTodoList: Todo[];
+
+    //assumes only following values : call, active, completed
+    //TODO : replace with enum
+    private viewMode: string = 'all';
 
     private newTodo = '';
 
     //Stores the count of todos yet to be completed
     private numActive = 0;
-    
+
     /**
      * Want to retrieve the TodoList from db so that we can edit it.
      * The _id is passed in via the route /edit-lists/:_id
@@ -68,6 +84,7 @@ export class EditListPageComponent implements OnInit {
                 this.currentTodoList = todoList
                 console.log('retrieved : ' + this.currentTodoList.title);
                 this.numActive = this.todoService.calculateNumActive(this.currentTodoList.todos);
+                this.filterByAll();
             }
             );
     }
@@ -84,6 +101,7 @@ export class EditListPageComponent implements OnInit {
                 this.currentTodoList = data;
                 this.newTodo = '';
                 this.numActive = this.todoService.calculateNumActive(this.currentTodoList.todos);
+                this.filterViewableList();
 
             });
 
@@ -99,6 +117,8 @@ export class EditListPageComponent implements OnInit {
                 console.log('bar list is now ' + JSON.stringify(data));
                 this.currentTodoList = data;
                 this.numActive = this.todoService.calculateNumActive(this.currentTodoList.todos);
+                this.filterViewableList();
+
             });
 
     }
@@ -115,8 +135,76 @@ export class EditListPageComponent implements OnInit {
                 console.log('bar list is now ' + JSON.stringify(data));
                 this.currentTodoList = data;
                 this.numActive = this.todoService.calculateNumActive(this.currentTodoList.todos);
+                this.filterViewableList();
+
             });
 
+    }
+
+    /**
+     * Sets view mode to 'all' and then resets view
+     */
+    showAll() {
+        this.viewMode = 'all';
+        this.filterViewableList();
+    }
+
+    /**
+     * Sets view mode to 'active' and then resets view
+     */
+    showActive() {
+        this.viewMode = 'active';
+        this.filterViewableList();
+    }
+
+    /**
+     * Sets view mode to 'active' and then resets view
+     */
+    showCompleted() {
+        this.viewMode = 'completed';
+        this.filterViewableList();
+    }
+
+    /**
+     * Filters the viewable dataset based on the value of viewMode.
+     */
+    filterViewableList() {
+        switch (this.viewMode) {
+            case 'all': this.filterByAll()
+                break;
+            case 'active': this.filterByActive();
+                break;
+            case 'completed': this.filterByCompleted();
+                break;
+            default:
+                console.log('Error - invalid viewMode value supplied');
+
+        }
+    }
+
+    /**
+     * resets the viewable dataset to that of the original
+     */
+    filterByAll() {
+        this.displayableTodoList = this.currentTodoList.todos;
+    }
+
+    /**
+     * resets the viewable dataset to only todos that have not been completed
+     */
+    filterByActive() {
+        this.displayableTodoList = this.currentTodoList.todos.filter(function (item) {
+            return !item.completed;
+        });
+    }
+
+    /**
+     * resets the viewable dataset to only todos that have been completed
+     */
+    filterByCompleted() {
+        this.displayableTodoList = this.currentTodoList.todos.filter(function (item) {
+            return item.completed;
+        });
     }
 
 }
